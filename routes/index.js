f4a28786554d094f57691fe0f127fe3fea35bbbf
Wally Lu <wlu@netspi.com>
2021-04-21 16:31:04 -0500

    Added in nodemon and sqlite db

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const { restart } = require('nodemon');
+const { exec } = require('../util/database');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });

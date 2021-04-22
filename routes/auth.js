e761db7aebb671f558623de346431f5251359a6a
Wally Lu <wlu@netspi.com>
2021-04-22 10:49:18 -0500

    Skeleton for api methods

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
+module.exports = router;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+
+/* POST /api/v2/auth/login */
+router.post('/api/v2/auth/login', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+
+/* POST /api/v2/auth/refresh */
+router.post('/api/v2/auth/refresh', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* POST /api/v2/auth/logout */
+router.post('/api/v2/auth/logout', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET  /api/v2/auth/history/{userID} */
+router.get('/api/v2/auth/history/userID', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 

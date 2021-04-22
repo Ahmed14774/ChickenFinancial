97168ed44826d3dfbb4a43241eedfe132c250005
DontTemptMeFrodo <andrew.mcintyre2014@gmail.com>
2021-04-22 13:39:20 -0500

    updated GET accounts with transfers

--- routes/account.js
+++ routes/account.js
@@ -8,16 +8,34 @@ router.get('/:account_id', function(req, res, next) {
   //Add SQL to retrieve account balance and history
   var sql = "select * from account where accountID = ?"
   var params = [req.params.account_id]
-  db.all(sql, params, (err, rows) => {
+  var userID = 0;
+  var accountName = "";
+  var numEgg = 0;
+  db.get(sql, params, (err, row) => {
     if (err) {
       res.status(400).json({"error": err.message})
       return;
-    } else if (!rows){
+    } else if (!row){
         return;
     } else{
-        res.send(rows)
+        userID = row.userID;
+        accountName = row.name;
+        numEgg = row.numEgg;
     }
   });
+
+  var sql1 = "select * from transfer where origin = ? or target = ?"
+  db.all(sql1, params, (err, rows) => {
+      if(err) {
+        res.status(400).json({"error": err.message})
+        return;
+      } else if (!rows){
+        return;
+      } else {
+        res.send(rows)
+      }
+  });
+
 });
 
 module.exports = router;

a009eeeb2cda67436e1ce49b85a0309512e1baa4
jlealazevedo <justin.l.azevedo@gmail.com>
2021-04-22 11:23:31 -0700

    Testing Commit

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET /api/v1/user/{UserID} (depricated)  //Get User Profile & Account List Based on Parameter */
+//Returns a complete list of accounts connected with the userID variable.
+//Vulnerbale to IDOR
+router.get('/api/v1/user/:userID', function(req, res, next){
+
+  //Get userID from URL
+
+  //Query SQL database for transactions
+  //SELECT * FROM Account WHERE userID =  
+  //var query = "SELECT * FROM Account WHERE userID = ?";
+  //var value = req.url.userID;
+  res.render('error', {message: 'test'});
+
+
+});
+
 module.exports = router;
+                    

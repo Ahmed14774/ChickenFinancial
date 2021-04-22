44d0fe93f8ae0e0d361f37246017b104c595a02a
Vesper Perry <vperry@netspi.com>
2021-04-22 12:57:23 -0500

    Add logic for transfer GET request

--- routes/transfer.js
+++ routes/transfer.js
@@ -3,11 +3,27 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 
 const { restart } = require('nodemon');
-const db = require('../util/database')
+const db = require('../util/database');
 
 /* GET a transfer by ID. */
 router.get('/:transfer_id', function(req, res, next) {
-  res.render('index', { title: 'Account' });
+
+  // Create and run query
+  query = "SELECT * FROM transfer WHERE transferID = ?";
+  
+  db.get(query, [req.params.transfer_id], function (err, row) {
+
+    // If error, return
+    if (err) {
+      res.status(400).json({ "Error": err.message });
+      return;
+    }
+
+    // If success, return data
+    res.json({
+      "transaction": row
+    })
+  });
 });
 
 /* Create new transfer. */
@@ -18,7 +34,7 @@ router.post('/', function(req,res) {
 /* Edit a transfer. */
 router.put('/:transfer_id', function(req, res) {
   // Transfer can only be edited if it hasn't been authorized
-  // VULN: allow users to change account IDs to accounts they don't own
+  // VULN: allow users to change account IDs to accounts they don't own (per Aaron)
 });
 
 /* Delete a transaction. */

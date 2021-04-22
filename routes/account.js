abfc9ff35e7b93100226ecdc3646bb59ab769191
DontTemptMeFrodo <andrew.mcintyre2014@gmail.com>
2021-04-22 12:59:49 -0500

    Base functionality for GET accounts

--- routes/account.js
+++ routes/account.js
@@ -1,10 +1,23 @@
 var express = require('express');
+const db = require('../util/database');
 var router = express.Router();
 
 //This is a test comment
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:account_id', function(req, res, next) {
+  //Add SQL to retrieve account balance and history
+  var sql = "select * from account where accountID = ?"
+  var params = [req.params.account_id]
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({"error": err.message})
+      return;
+    } else if (!rows){
+        return;
+    } else{
+        res.send(rows)
+    }
+  });
 });
 
 module.exports = router;
